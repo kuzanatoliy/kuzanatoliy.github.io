651620690c8565c93a53b34e31a31314f15b6927
kuzanatoliy <njkznu@gmail.com>
2016-12-16 14:38:35 +0300

    changed the errors

---
+++ sudocu/kuzan.sudocu/kuzan.sudocu.js
@@ -0,0 +1,12 @@
+var kuzanSudocu = (function($){
+    var modulName = 'kuzan.sudocu';
+    var modulId = 'kuzan.sudocu';
+    
+    var sudocu = {};
+    
+    sudocu.create = function(){
+        
+    }
+    
+    return sudocu;
+}(jQuery));
